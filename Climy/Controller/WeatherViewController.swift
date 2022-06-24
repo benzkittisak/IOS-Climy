275ca7a6327570fbe18cc39a74f7279ee89ddeba
benzkittisak <benzkittisak@stv.ac.th>
2022-06-24 11:55:02 +0700

    Initial Commit

---
+++ Climy/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Climy
+//
+//  Created by Kittisak Panluea on 24/6/2565 BE.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionalIV: UIImageView!
+    @IBOutlet weak var temperatureLB: UILabel!
+    @IBOutlet weak var cityLB: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
